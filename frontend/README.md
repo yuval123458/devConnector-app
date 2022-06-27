931d99c55d959145415e9425fb057c8f9bc4b755
יובל דויטש <yuvaldeutche@h-MacBook-Pro-sl-ywbl.local>
2022-06-27 17:08:50 +0300

    initial frontend with backend

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+# linkedin-mern-app
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import react, { Fragment } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>APP</h1>
+    </Fragment>
+  );
+};
+export default App;
